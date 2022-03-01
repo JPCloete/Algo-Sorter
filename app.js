c0a1b815f7bb775527f7bf846374c79d1eef8551
JP Cloete <jpcloetedev@gmail.com>
2022-03-01 15:15:04 +0200

    Updated comments in app.js

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-/* 
-Sources:
-https://www.youtube.com/watch?v=67k3I2GxTH8&t
-https://www.youtube.com/watch?v=eqo2LxRADhU&t
-
-SPECIAL THANKS: 
-Clément Mihailescu for inspiring me^1 to make AlgoSorter, and his guided implementation of the merge sort algorhytm^2 that helped immensely with the 
-implementation of the quick -and bubble sort.      
-
-1: https://www.youtube.com/watch?v=n4t_-NjY_Sg&t
-2: https://www.youtube.com/watch?v=pFXYym4Wbkc
-*/
-
 const arrayLength = 120;
 var flagResetButton = true;
 var flagIsReset = true;
